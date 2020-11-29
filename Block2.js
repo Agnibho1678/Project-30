79efb85e56ed5a479907149c2095102ec18594e1
Agnibho1678 <73741624+Agnibho1678@users.noreply.github.com>
2020-11-29 18:22:54 +0530

    Update Block2.js

--- Block2.js
+++ Block2.js
@@ -29,6 +29,7 @@ class Block2{
         World.remove(world, this.body);
         push();
         this.visibility = this.visibility - 5;
+        tint(255,this.Visiblity);
         pop();
     }
     }
