3b4b3a66916b0770e03dd9bc279bd29fb26d8def
Agnibho1678 <73741624+Agnibho1678@users.noreply.github.com>
2020-11-29 18:30:16 +0530

    Update Block1.js

--- Block1.js
+++ Block1.js
@@ -5,7 +5,7 @@ class Block1{
             restituition: 0.4,
             friction:0.0
         }
-        this.visibility = 225;
+        this.Visibility = 225;
         this.body = Bodies.rectangle(x,y,width,height,options);
         this.width=width;
         this.height=height;
@@ -28,8 +28,8 @@ class Block1{
         }else{
             World.remove(world, this.body);
             push();
-            this.visibility = this.visibility - 5;
-            tint(255,this.visiblity);
+            this.Visibility = this.Visibility - 5;
+            tint(255,this.Visiblity);
             pop();
         }
         
